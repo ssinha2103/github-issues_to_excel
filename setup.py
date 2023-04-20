c817f9528e50ccfc9961002021b657f3701f9a43
ssinha2103 <s.sinha2103@outlook.com>
2023-04-21 02:07:35 +0530

    added setup.py for configuration of the project

--- setup.py
+++ setup.py
@@ -1,30 +1,27 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-    name="GitHubIssueExtractor",
-    version="1.0.0",
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="github_issue_extractor",
+    version="0.1.0",
+    author="Sudarshan Sinha",
+    author_email="s.sinha2103@outlook.com",
     description="GitHubIssueExtractor",
-    packages=find_packages(),
-    install_requires=[
-        "certifi==2022.12.7",
-        "charset-normalizer==3.1.0",
-        "et-xmlfile==1.1.0",
-        "idna==3.4",
-        "openpyxl==3.1.2",
-        "requests==2.28.2",
-        "urllib3==1.26.15",
-        "twine==4.0.2"
-    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ssinha2103/github-issues_to_excel",
+    packages=setuptools.find_packages(),
     classifiers=[
-        "Development Status :: Production",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
+    install_requires=[
+        "setuptools",
+        "requests",
+        "openpyxl"
+    ]
 )
